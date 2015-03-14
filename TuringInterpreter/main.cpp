c959d2d5b98c820bee88cfe948d8a47fb6a4259c
Andrea Zagar <andrea@Palantir.local>
2015-03-14 14:30:13 -0400

    Initial Commit

---
+++ TuringInterpreter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TuringInterpreter
+//
+//  Created by Andrea Zagar on 2015-03-14.
+//  Copyright (c) 2015 Andrea Zagar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
