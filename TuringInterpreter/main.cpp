c49295be57bafebed4575fc841bc7f8de7a5185e
Andrea Zagar <andrea@Palantir.local>
2015-03-15 18:58:00 -0400

    Fully functional except for outputs

--- TuringInterpreter/main.cpp
+++ TuringInterpreter/main.cpp
@@ -7,9 +7,197 @@
 //
 
 #include <iostream>
+#include <map>
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//struct State {
+//    int currentState;
+//    char input;
+//    
+//    State() {};
+//    State(char s, char i) {
+//        currentState = s;
+//        input = i;
+//    }
+//    
+//    bool operator<( const State & n ) const {
+//        return this->input < n.input;   // for example
+//    }
+//};
+
+struct Transition {
+    char next;
+    char output;
+    char direction;
+    
+    Transition() {};
+    Transition(char s, char o, char d) {
+        next = s;
+        output = o;
+        direction = d;
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    typedef vector<char> vecList;
+    vecList finalStates;
+    vector<vecList> inputs;
+    
+    // Map types
+    typedef map<char, Transition> inputTransition;
+    map<char, inputTransition> m;
+    //map<State, Transition> m;
+
+    // Read from file
+    string line;
+    ifstream myfile ("input.txt");
+    if (myfile.is_open())
+    {
+        while (getline (myfile,line))
+        {
+            string s = "";
+            for ( int i = 0 ; i < line.length(); i++)
+            {
+                if (!isspace(line[i]))
+                    s += line[i];
+            }
+            
+            if (s[0] == 't') { // transition
+                
+                inputTransition tempMap;
+                //tempMap[s[2]] = Transition(s[3], s[4], s[5]); // add to inputTransition map
+                m[s[1]][s[2]] = Transition(s[3], s[4], s[5]);
+            }
+            
+            else if (s[0] == 'f') { // final states
+                for (int i = 1; i < s.length(); i++)
+                {
+                    finalStates.push_back(s[i]);
+                }
+            }
+            
+            else { // input string
+                vector<char> newString;
+                newString.push_back('Z'); // begin with Z
+                for (int i = 1; i < s.length(); i++)
+                {
+                    newString.push_back(s[i]);
+                }
+                newString.push_back('Z'); // end with Z
+                inputs.push_back(newString);
+//                for (vecList::iterator it = newString.begin(); it != newString.end(); it++)
+//                    std::cout << *it;
+//                cout << endl;
+            }
+        }
+        myfile.close();
+
+        // TESTING TO SEE ALL STATES
+//        for(map<char, inputTransition>::iterator it = m.begin(); it != m.end(); ++it) {
+//            cout << "state: " << it->first << "\n";
+//            inputTransition temp = it->second;
+//            for(inputTransition::iterator it = temp.begin(); it != temp.end(); ++it) {
+//                cout << "symbol: " << it->first << "\n";
+//            }
+//        }
+        
+    }
+    else cout << "Unable to open file";
+    
+    
+    /* TURING MACHINE */
+    
+    cout << inputs.size() << endl;
+    
+    for (int i = 0; i < inputs.size(); i++)
+    {
+        vecList tape = inputs[i];
+        
+        vecList::iterator it = tape.begin();
+        it++; // skips the first element which is Z
+        
+        string output = "";
+        char currentState, nextState = '0';
+        
+        while (true)
+        {
+            currentState = nextState;
+            //cout << "The current symbol is: " << *(it) << endl;
+            
+            // Check if input symbol is valid
+            if (! m[currentState].count(*(it)))
+            {
+                for (vecList::iterator it = tape.begin() + 1; it != tape.end() - 1; it++) {
+                    output += *(it);
+                }
+                cout << output << endl;
+                cout << "REJECTED" << endl;
+                break;
+                // need to add on the rest of the input symbols to the output...
+            }
+            
+            // Check if direction is H
+            if (m[currentState][*(it)].direction == 'H')
+            {
+                nextState = m[currentState][*(it)].next;
+                // Add everything to the output string
+                for (vecList::iterator it = tape.begin() + 1; it != tape.end() - 1; it++) {
+                    output += *(it);
+                }
+                cout << output << endl;
+                if(find(finalStates.begin(), finalStates.end(), nextState) == finalStates.end())
+                    cout << "Not a final state" << endl;
+                else
+                    cout << "ACCEPT" << endl;
+                break;
+            }
+            
+            // Set the next state
+            nextState = m[currentState][*(it)].next;
+            //cout << "next state is: " << nextState << endl;
+            char symbol = *(it);
+            
+            // Modify the tape
+            *(it) = m[currentState][symbol].output;
+            
+            // Set next direction
+            if (m[currentState][symbol].direction == 'L')
+            {
+                //cout << "go left" << endl;
+                it--;
+            }
+            else if (m[currentState][symbol].direction == 'R')
+            {
+                //cout << "go right" << endl;
+                it++;
+            }
+            
+        }
+        
+    }
+    
+    
     return 0;
 }
+
+
+// set current state
+// read input symbol
+// check if Z
+// add to output string
+// set current state to next state
+// move iterator right or left depending on R or L (for now increment every time)
+
+
+
+
+
+
